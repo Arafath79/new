f0149dac0214e1aa99dc2d906df555605f4aa729
Your Name <you@example.com>
2018-02-18 00:42:39 -0800

    my class

---
+++ js/main.js
@@ -0,0 +1,15 @@
+ $(document).ready(function() {
+     
+      $("#owl-demo").owlCarousel({
+     
+          navigation : true, // Show next and prev buttons
+          slideSpeed : 300,
+          paginationSpeed : 400,
+          singleItem:true,
+		  autoPlay: 3000,
+		  pagination:false,
+          navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
+    });
+     
+    });
+
